6f018595dbfcf80a34abffc9d3031ad0b3ffd87e
Benjamin Yan <benyanc@gmail.com>
2017-01-19 20:55:17 +0800

    added features to select number of picks, and minimize/mute

--- NewYearsPartyGame/NewYearsPartyGame/MainWindow.xaml.cs
+++ NewYearsPartyGame/NewYearsPartyGame/MainWindow.xaml.cs
@@ -25,8 +25,8 @@ namespace NewYearsPartyGame
     /// </summary>
     public partial class MainWindow : Window
     {
-        private MediaPlayer bgmMP;
-        private MediaPlayer actionMP;
+        private MediaPlayer bgmMP = new MediaPlayer();
+        private MediaPlayer actionMP = new MediaPlayer();
         private int empsPerRound = 20;
         private int totalCountdownTime = 15;
         private string[] bgmFileNames = { "audio/bgm_0.mp3", "audio/bgm_1.mp3", "audio/bgm_2.mp3" };
@@ -42,8 +42,8 @@ namespace NewYearsPartyGame
         private List<Employee> displayedEmpList = new List<Employee>();
         private List<Grid> employeesChosenGridList = new List<Grid>();
         Employee currEmp = null;
-        private double photoWidth;
-        private double photoHeight;
+        private double photoWidth = 1;
+        private double photoHeight = 1;
         private Duration translateDuration = new Duration(new TimeSpan(0, 0, 0, 5, 0));
         private List<double> chosenEmpsXCoords = new List<double>();
         private double[] chosenEmpsYCoords;
@@ -84,9 +84,6 @@ namespace NewYearsPartyGame
             timerTextBox.Height = photoHeight;
             timerTextBox.TextAlignment = TextAlignment.Center;
 
-
-            bgmMP = new MediaPlayer();
-
             bgmMP.MediaEnded += (o, args) =>
             {
                 bgmMP.Open(new Uri(bgmFileNames[currentBgmIndex], UriKind.RelativeOrAbsolute));
@@ -104,8 +101,6 @@ namespace NewYearsPartyGame
         private void CycleEmployeePhotos(object sender, EventArgs e)
         {
             displayedEmpList.Clear();
-            if (employeesList.Count() < empsPerRound)
-                empsPerRound = employeesList.Count();
             while (displayedEmpList.Count() < empsPerRound)
             {
                 int randomIndex = rnd.Next(employeesList.Count());
@@ -134,15 +129,15 @@ namespace NewYearsPartyGame
             //make three rows of seven pics each; bottom right one is for timer
             for (int i = 0; i < 7; i++)
                 chosenEmpsXCoords.Add(xMargin + (photoWidth + xSeparation) * i);
-            chosenEmpsYCoords = new Double[] { ySeparation, ySeparation + photoHeight + ySeparation, ySeparation + (photoHeight + ySeparation) * 2};
+            chosenEmpsYCoords = new Double[] { ySeparation, ySeparation + photoHeight + ySeparation, ySeparation + (photoHeight + ySeparation) * 2 };
 
 
             employeesChosenGridList = new List<Grid>();
-            foreach (Double x in chosenEmpsXCoords)
+            foreach (Double y in chosenEmpsYCoords)
             {
-                foreach (Double y in chosenEmpsYCoords)
+                foreach (Double x in chosenEmpsXCoords)
                 {
-                    if (chosenEmpsXCoords.IndexOf(x)==6 && chosenEmpsYCoords.ElementAt(2) == y)
+                    if (chosenEmpsXCoords.IndexOf(x) == 6 && chosenEmpsYCoords.ElementAt(2) == y)
                     {
                         timerTextBox.Margin = new Thickness(x, y, 0, 0);
                         continue;
@@ -159,9 +154,9 @@ namespace NewYearsPartyGame
 
                     TextBox txbx = new TextBox();
                     txbx.Name = "name";
-                    txbx.Height = photoHeight / 5;
+                    txbx.Height = photoHeight / 6;
                     txbx.Width = photoWidth;
-                    txbx.FontSize = 18;
+                    txbx.FontSize = 20;
                     txbx.FontWeight = FontWeights.Bold;
                     txbx.Background = Brushes.DeepSkyBlue;
                     txbx.Foreground = Brushes.White;
@@ -190,8 +185,10 @@ namespace NewYearsPartyGame
             if (inActionAnimation || employeesList.Count <= 0)
                 return;
 
+            slider.Visibility = Visibility.Hidden;
             inActionAnimation = true;
             cycleTimer.Stop();
+
             foreach (Employee emp in displayedEmpList)
                 employeesList.Remove(emp);
 
@@ -212,6 +209,7 @@ namespace NewYearsPartyGame
                       inActionAnimation = false;
                       actionMP.Stop();
                       bgmMP.Play();
+                      slider.Visibility = Visibility.Visible;
                   }
                   _time--;
               };
@@ -219,7 +217,6 @@ namespace NewYearsPartyGame
             runningTimer.Start();
 
             bgmMP.Pause();
-            actionMP = new MediaPlayer();
             actionMP.Open(new Uri(@"audio/stadium.mp3", UriKind.RelativeOrAbsolute));
             actionMP.Play();
 
@@ -233,178 +230,40 @@ namespace NewYearsPartyGame
         {
             if (inCycleAnimation || inActionAnimation || employeesList.Count <= 0)
                 return;
+
+            if (employeesList.Count() < empsPerRound)
+            {
+                empsPerRound = employeesList.Count();
+            }
+            for (int i = empsPerRound; i < 20; i++)
+                employeesChosenGridList[i].Visibility = Visibility.Hidden;
+
+            for (int i = 0; i < empsPerRound; i++)
+                employeesChosenGridList[i].Visibility = Visibility.Visible;
+
+
+            slider.Visibility = Visibility.Hidden;
             inCycleAnimation = true;
             cycleTimer = new DispatcherTimer(DispatcherPriority.Render);
             cycleTimer.Interval = TimeSpan.FromSeconds(.1);
             cycleTimer.Tick += CycleEmployeePhotos;
             cycleTimer.Start();
-
         }
 
-        /*
-        private void ShowEmp(object sender, EventArgs e)
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            //if (currEmp != null)
-            //{
-            //}
-            if (empsChosen >= empsPerRound || employeesList.Count() == 0)
-            {
-                currEmp = null;
-                inChoosingEmps = true;
-                empsChosen = 0;
-                return;
-            }
-
-            Random rnd = new Random();
-            int indexChosen = rnd.Next(0, employeesList.Count());
-            currEmp = employeesList[indexChosen];
-
-            double deltaX = chosenEmpsXCoords[empsChosen] - currEmp.X;
-            double deltaY;
-            if (empsChosen < empsPerRound / 2)
-                deltaY = chosenEmpsYCoords[0] - currEmp.Y;
-            else
-                deltaY = chosenEmpsYCoords[1] - currEmp.Y;
-
-
-            TranslateTransform ttx = new TranslateTransform();
-            TranslateTransform tty = new TranslateTransform();
-            DoubleAnimation dax = new DoubleAnimation();
-            DoubleAnimation day = new DoubleAnimation();
-            //dax = new DoubleAnimation(currEmp.X, chosenEmpsXCoords[empsChosen], new Duration(TimeSpan.FromSeconds(1)));
-            dax = new DoubleAnimation(deltaX, new Duration(TimeSpan.FromSeconds(.5)));
-            //if (empsChosen < empsPerRound / 2)
-            //    day = new DoubleAnimation(currEmp.Y, chosenEmpsYCoords[0], new Duration(TimeSpan.FromSeconds(1)));
-            //else
-            //    day = new DoubleAnimation(currEmp.Y, chosenEmpsYCoords[1], new Duration(TimeSpan.FromSeconds(1)));
-            day = new DoubleAnimation(deltaY, new Duration(TimeSpan.FromSeconds(.5)));
-            TransformGroup tg = new TransformGroup();
-            tg.Children.Add(ttx);
-            tg.Children.Add(tty);
-
-            dax.Completed += (o, args) =>
-            {
-                BitmapImage img;
-                try
-                {
-                    img = new BitmapImage(new Uri(currEmp.PhotoPath, UriKind.RelativeOrAbsolute));
-                }
-                catch (Exception err)
-                {
-                    Console.WriteLine(err.Message);
-                    img = new BitmapImage(new Uri(@"images/中国结.jpg", UriKind.RelativeOrAbsolute));
-                }
-
-                //(currEmp.Grid.Children[1] as Ellipse).Fill = new ImageBrush(img);
-                (employeesChosenGridList[empsChosen].Children[0] as Ellipse).Fill = new ImageBrush(img);
-                (employeesChosenGridList[empsChosen].Children[1] as TextBox).Text = currEmp.Name;
-                currEmp.Grid.Children[0].Visibility = Visibility.Hidden;//circle
-                employeesChosenGridList[empsChosen].Visibility = Visibility.Visible;
-                //employeesChosenGridList[empsChosen].Children[0].Visibility = Visibility.Visible;//ellipse
-                //employeesChosenGridList[empsChosen].Children[1].Visibility = Visibility.Visible;//textbox
-                empsChosen++;
-                employeesList.Remove(currEmp);
-
-            };
-            dax.Completed += new EventHandler(ShowEmp);
-
-            currEmp.Grid.RenderTransform = tg;
-            ttx.BeginAnimation(TranslateTransform.XProperty, dax);
-            tty.BeginAnimation(TranslateTransform.YProperty, day);
-
+            empsPerRound = (int) slider.Value;
         }
-        */
-        /*
-        private void TranscriptaseGrabEmp(object sender, EventArgs e)
+
+        private void muteButton_Click(object sender, RoutedEventArgs e)
         {
-            if (employeesChosenList.Count() >= empsPerRound)
-            {
-                //bgmMP.Play();
-                inChoosingEmps = false;
-                return;
-            }
-
-            Random rnd = new Random();
-            int indexChosen = rnd.Next(0, employeesList.Count());
-            currEmp = employeesList[indexChosen];
-
-            Point transcriptasePoint = transcriptase.TransformToAncestor(actionArea).Transform(new Point(0, 0));
-            double deltaX = currEmp.X - transcriptasePoint.X;
-            double deltaY = currEmp.Y - transcriptasePoint.Y;
-
-            ttx = new TranslateTransform();
-            tty = new TranslateTransform();
-            //dax = new DoubleAnimation(transcriptasePoint.X, currEmp.X, new Duration(TimeSpan.FromSeconds(1)));
-            //day = new DoubleAnimation(transcriptasePoint.Y, currEmp.Y, new Duration(TimeSpan.FromSeconds(1)));
-            dax = new DoubleAnimation(deltaX, new Duration(TimeSpan.FromSeconds(1)));
-            day = new DoubleAnimation(deltaY, new Duration(TimeSpan.FromSeconds(1)));
-            tg = new TransformGroup();
-            tg.Children.Add(ttx);
-            tg.Children.Add(tty);
-
-            dax.Completed += new EventHandler(TranscriptaseMoveEmp);
-
-            transcriptase.RenderTransform = tg;
-            ttx.BeginAnimation(TranslateTransform.XProperty, dax);
-            tty.BeginAnimation(TranslateTransform.YProperty, day);
-
+            bgmMP.IsMuted = !bgmMP.IsMuted;
+            actionMP.IsMuted = !actionMP.IsMuted;
         }
 
-        private void TranscriptaseMoveEmp(object sender, EventArgs e)
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
         {
-            //Canvas.SetLeft(transcriptase, currEmp.X);
-            //Canvas.SetTop(transcriptase, currEmp.Y);
-
-            int empsChosen = employeesChosenList.Count();
-            BitmapImage img;
-            try
-            {
-                img = new BitmapImage(new Uri(currEmp.PhotoPath, UriKind.RelativeOrAbsolute));
-            }
-            catch (Exception err)
-            {
-                Console.WriteLine(err.Message);
-                img = new BitmapImage(new Uri(@"images/中国结.jpg", UriKind.RelativeOrAbsolute));
-            }
-            (currEmp.Grid.Children[1] as Ellipse).Fill = new ImageBrush(img);
-
-            currEmp.Grid.Children[0].Visibility = Visibility.Hidden;//circle
-            currEmp.Grid.Children[1].Visibility = Visibility.Visible;//ellipse
-            currEmp.Grid.Children[2].Visibility = Visibility.Visible;//textbox
-
-            double deltaX = chosenEmpsXCoords[empsChosen] - currEmp.X;
-            double deltaY;
-            if (empsChosen < empsPerRound / 2)
-                deltaY = chosenEmpsYCoords[0] - currEmp.Y;
-            else
-                deltaY = chosenEmpsYCoords[1] - currEmp.Y;
-
-
-            TranslateTransform ttx = new TranslateTransform();
-            TranslateTransform tty = new TranslateTransform();
-            DoubleAnimation dax = new DoubleAnimation();
-            DoubleAnimation day = new DoubleAnimation();
-            //dax = new DoubleAnimation(currEmp.X, chosenEmpsXCoords[empsChosen], new Duration(TimeSpan.FromSeconds(1)));
-            dax = new DoubleAnimation(deltaX, new Duration(TimeSpan.FromSeconds(1)));
-            //if (empsChosen < empsPerRound / 2)
-            //    day = new DoubleAnimation(currEmp.Y, chosenEmpsYCoords[0], new Duration(TimeSpan.FromSeconds(1)));
-            //else
-            //    day = new DoubleAnimation(currEmp.Y, chosenEmpsYCoords[1], new Duration(TimeSpan.FromSeconds(1)));
-            day = new DoubleAnimation(deltaY, new Duration(TimeSpan.FromSeconds(1)));
-            TransformGroup tg = new TransformGroup();
-            tg.Children.Add(ttx);
-            tg.Children.Add(tty);
-
-            dax.Completed += new EventHandler(TranscriptaseGrabEmp);
-
-            currEmp.Grid.RenderTransform = tg;
-            transcriptase.RenderTransform = tg;
-            ttx.BeginAnimation(TranslateTransform.XProperty, dax);
-            tty.BeginAnimation(TranslateTransform.YProperty, day);
-
-            employeesChosenList.Add(currEmp);
-            employeesList.Remove(currEmp);
+            mainWindow.WindowState = WindowState.Minimized;
         }
-        */
     }
 }
